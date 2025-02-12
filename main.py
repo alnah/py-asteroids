2b2698bfd890c1d8e0b729d386f095e9ee6da184
alnah <alexis.nahan@gmail.com>
2025-02-12 16:10:18 -0300

    feat: limit framerate to 60 fps

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame
-from constants import COLOR, SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import COLOR, FRAMERATE, SCREEN_WIDTH, SCREEN_HEIGHT
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -13,6 +15,9 @@ def main():
 
         screen.fill(COLOR)
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(FRAMERATE) / 1000
 
 
 if __name__ == "__main__":
