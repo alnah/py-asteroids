d2380009b33ddfe47b4623eabf7d557582e75233
alnah <alexis.nahan@gmail.com>
2025-02-12 16:15:21 -0300

    feat: add circle shape

---
+++ circleshape.py
@@ -0,0 +1,21 @@
+import pygame
+
+
+# Base class for game objects
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x: int, y: int, radius: int) -> None:
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+        self.position: pygame.Vector2 = pygame.Vector2(x, y)
+        self.velocity: pygame.Vector2 = pygame.Vector2(0, 0)
+        self.radius: int = radius
+
+    def draw(self, screen: pygame.Surface) -> None:
+        # sub-classes must override
+        pass
+
+    def update(self, dt: int) -> None:
+        # sub-classes must override
+        pass
